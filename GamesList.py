50e73c0931caad1f9712954ce04616fd4fb61fc8
BertrandHustle <scott.greenberg926@gmail.com>
2020-04-16 20:35:12 -0400

    part numbers without hyphens are now scraped properly

--- GamesList.py
+++ GamesList.py
@@ -1,3 +1,5 @@
+# native
+import re
 # project
 from PartNumberFinder import PartNumberFinder
 
@@ -17,12 +19,17 @@ class GamesList:
         for pair in PartNumberFinder.get_game_names_and_part_numbers(url):
             self.games_list[pair[0]] = pair[1]
 
+    # TODO: this needs to put a hyphen between the letters and numbers
     def get_name_by_part_number(self, part_number: str) -> str:
         """
         gets game name given a part number
         :return str: name
         """
         try:
+            if re.match(r'[A-Z]{4}\d{5}', part_number):
+                part_number = ''.join([c for c in part_number if c.isalpha()]) \
+                              + '-' \
+                              + ''.join([c for c in part_number if c.isdigit()])
             return self.games_list[part_number]
         # TODO: have this output to log
         except KeyError:
