093448041b114096f71328436d81a5ab370edfa9
BertrandHustle <scott.greenberg926@gmail.com>
2020-04-30 21:35:13 -0400

    converted main to script and changed to read game title directly from eboot file

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 # native
 import argparse
 import os
-# project
-import GamesList
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dir", help="directory to scrape")
 
 args = parser.parse_args()
-dir_to_scrape = os.pardir
+dir_to_scrape = os.curdir
 if args.dir:
     dir_to_scrape = args.dir
 
-folders = os.listdir(dir_to_scrape)
+folders = [d for d in os.listdir(dir_to_scrape) if os.path.isdir(d)]
 
-games_list = GamesList.GamesList()
 
-urls = [
-    'https://psxdatacenter.com/ulist.html',
-    'https://psxdatacenter.com/plist.html',
-    'https://psxdatacenter.com/jlist.html'
-    ]
+def get_game_name(filename):
+    gamename = ''
+    with open(filename, 'rb') as eboot:
+        eboot.seek(int('0x358', base=16))
+        while True:
+            try:
+                gamename += eboot.read(1).decode()
+            except UnicodeDecodeError:
+                return gamename.replace(' ', '')
 
-for url in urls:
-    games_list.populate_games_list(url)
 
 for folder in folders:
-    lookup_result = games_list.get_name_by_part_number(folder)
-    if lookup_result:
-        os.rename(os.path.join(dir_to_scrape, folder), os.path.join(dir_to_scrape, lookup_result))
+    folder_path = os.path.join(dir_to_scrape, folder)
+    eboot_path = os.path.join(folder_path, 'EBOOT.PBP')
+    contains_eboot = 'EBOOT.PBP' in os.listdir(folder_path)
+    new_folder_name = ''
+    if contains_eboot:
+        new_folder_name = get_game_name(eboot_path)
+        new_folder_path = os.path.join(dir_to_scrape, new_folder_name)
+        os.rename(folder_path, new_folder_path)
+    else:
+        pass
+        # print(folder_path + ' does not contain EBOOT.PBP!')
