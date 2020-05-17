7dcd1b6df4cfb6227cbc9cb0c7a4809254b46135
BertrandHustle <scott.greenberg926@gmail.com>
2020-05-17 15:52:58 -0400

    added GPLv3 license

--- psx_scraper.py
+++ psx_scraper.py
@@ -3,6 +3,23 @@
 import argparse
 import os
 
+"""
+    Copyright (C) 2020  Scott Greenberg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dir', help='directory to scrape')
 parser.add_argument('-l', '--log', help='enables logging', action='store_true')
